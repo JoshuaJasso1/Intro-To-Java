eb25edef41b9c39751aafc3040c7afb492a6959a
JoshuaJasso1 <joshuajasso1@gmail.com>
2017-07-19 11:55:43 -0700

    Add files via upload

---
+++ MyFirstJavaProgram.java
@@ -0,0 +1,48 @@
+package day1.robot;
+
+import java.awt.Color;
+import org.jointheleague.graphical.robot.Robot;
+
+public class MyFirstJavaProgram {
+	public static void main(String[] args) {
+		
+		// START HERE
+		Robot r2d2= new Robot();
+		r2d2.penDown();
+		r2d2.setRandomPenColor();
+		for (int i = 0; i < 3; i++) {
+			r2d2.move(300);
+			r2d2.turn(120);
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
